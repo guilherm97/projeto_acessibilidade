0279bc8edfe78769faf4df3a036d6a610e17a8dd
Guilherme Da silva Barbarino <74333587+guilherm97@users.noreply.github.com>
2023-02-28 10:28:37 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 'use strict';
-
+const nameDigitado = document.querySelector('name');
 const switcher = document.querySelector('.btn');
-switcher.addEventListener('click', function () {
 
+
+switcher.addEventListener('click', function() {
     document.body.classList.toggle('tema_claro');
     document.body.classList.toggle('tema_escuro');
 
     const className = document.body.className;
-
-    if (className == "tema_claro") {
-
+    if(className == "tema_claro") {
         this.textContent = "Apagar a luz";
+        
     } else {
         this.textContent = "Acender a luz";
     }
+
     console.log('nome da classe atual: ' + className);
 });
+
+
+
